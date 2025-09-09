cddf8c9e693f54dd452a7420f0d860d7003fd649
Rodrigo Castro <rodrigo.castro.v@utec.edu.pe>
2025-09-08 20:32:46 -0500

    feat(deploy): setup del front y server implementado

--- deploy_local.sh
+++ deploy_local.sh
@@ -8,6 +8,13 @@ command_exists() {
 # Detener la ejecucion si un comando falla
 set -e
 
+# Funcion para limpiar procesos en segundo plano al salir del script
+cleanup() {
+    echo "Deteniendo todos los servidores..."
+    kill 0
+}
+trap cleanup EXIT
+
 echo "Iniciando despliegue local del Sistema POS..."
 
 # --- Verificacion de Dependencias ---
@@ -41,18 +48,39 @@ source venv/Scripts/activate  # Esto es para git bash en windows por eso usa Scr
 pip install -r requirements.txt
 deactivate 
 cd .. 
-echo "Backend configurado."
 
-echo "Backend configurado"
+echo "Backend configurado."
 
 
 # --- Configuración Frontend ---
 echo "Configurando entorno del frontend..."
 
+cd frontend
+if [ ! -d "node_modules" ]; then
+    echo "Instalando dependencias de Node.js"
+    npm install
+else
+    echo "Dependencias de Node.js ya instaladas."
+fi
+cd ..
+
 echo "Frontend configurado"
 
 
-# --- Finalizacion ---
-echo "Despliegue exitoso"
+# --- Levantando servidores ---
+echo "Levantando los servidores"
 
-#Base del script hecho, logica real se implementara despues
+# Iniciar backend en segundo plano
+echo "Iniciando servidor backend en segundo plano (http://127.0.0.1:5000)."
+
+# Ocurre en el subshell, basicamente se ejecuta en segundo plano para poder seguir usando terminal
+(cd backend && source venv/Scripts/activate && flask run &)
+
+# Unos segundos para que el backend inicie
+sleep 3
+
+# Iniciar frontend en primer plano
+echo "Iniciando servidor frontend"
+echo "Aplicacion disponible en http://localhost:3000"
+echo "Presiona Ctrl + C en esta ventana para cerrar"
+(cd frontend && node server.js)
