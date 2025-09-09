bc3d0b4bf62625ac68017814af8e10b8e57537de
Rodrigo Castro <rodrigo.castro.v@utec.edu.pe>
2025-09-08 20:20:24 -0500

    feat(deploy): Configuracion backend

--- deploy_local.sh
+++ deploy_local.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# El comando existe?
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
 # Detener la ejecucion si un comando falla
 set -e
 
@@ -8,11 +13,35 @@ echo "Iniciando despliegue local del Sistema POS..."
 # --- Verificacion de Dependencias ---
 echo "Verificando dependencias"
 
+dependencies=("git" "python" "node")
+
+for cmd in "${dependencies[@]}"; do
+    if command_exists "$cmd"; then
+        echo "$cmd está instalado."
+    else
+        echo "Error: $cmd no está instalado. Instalar pls."
+        exit 1
+    fi
+done
+
 echo "Verificación de dependencias completada"
 
 
 # --- Configuracion Backend ---
 echo "Configurando entorno del backend..."
+
+cd backend
+if [ ! -d "venv" ]; then
+    echo "Creando entorno virtual..."
+    python -m venv venv
+fi
+
+echo "Instalando dependencias de Python..."
+source venv/Scripts/activate  # Esto es para git bash en windows por eso usa Scripts en vez de bin.
+pip install -r requirements.txt
+deactivate 
+cd .. 
+echo "Backend configurado."
 
 echo "Backend configurado"
 
